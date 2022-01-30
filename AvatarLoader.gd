834e9966ff648961f0c904cabfa440f6729c8541
Sercan Altundas <sercan.altundas@gmail.com>
2022-01-30 22:32:26 +0200

    remove httprequest from scene

--- AvatarLoader.gd
+++ AvatarLoader.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var anim_node: AnimationPlayer
 var avatar: Node3D
+var http_request: HTTPRequest = HTTPRequest.new()
 
 func _on_button_pressed():
 	if avatar != null:
@@ -16,11 +17,15 @@ func _on_button_pressed():
 		$Control/Button.disabled = true
 		$Control/Button.text = "Loading..."
 		
-		var error = $HTTPRequest.request($Control/Text.text)
+		add_child(http_request)
+		http_request.connect("request_completed", _request_completed)
+		var error = http_request.request($Control/Text.text)
 		if error != OK:
 			push_error("An error occurred in the HTTP request.")
+			$Control/Button.disabled = false
+			$Control/Button.text = "Load Avatar"
 
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+func _request_completed(result, response_code, headers, body):
 	var gltf = GLTFDocument.new()
 	var state = GLTFState.new()
 	var error = gltf.append_from_buffer(body, "", state)
